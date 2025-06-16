6e7ce50af18b654e8ff3ddf4f8aa73a9e894260b
Jinius36 <jinius0306@gmail.com>
2025-06-16 21:51:22 +0900

    Revert "키보드 구현, 버그 존재"
    This reverts commit 3a3302eb0991f68b0ec7faf9abba61b659e33052.

--- MyKeyboard/app/src/main/java/com/myhome/rpgkeyboard/MainActivity.kt
+++ MyKeyboard/app/src/main/java/com/myhome/rpgkeyboard/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.myhome.rpgkeyboard
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // 예시: 설정 화면 상단의 “제출” 버튼을 숨기고 싶다면
-        // val settingHeader = findViewById<View>(R.id.setting_header)
-        // val submitButton = settingHeader.findViewById<TextView>(R.id.submit_text)
-        // submitButton.visibility = View.GONE
+
+        val settingHeader = findViewById<ConstraintLayout>(R.id.setting_header)
+        val submitButton = settingHeader.findViewById<TextView>(R.id.submit_text)
+        submitButton.visibility = View.GONE
+
+
     }
+
+
+
+
 }
